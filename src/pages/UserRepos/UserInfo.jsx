65e6d7c076e4e0cf0fccb4690a5a2a7356b6f4e8
Adi52 <adimr52@gmail.com>
2020-12-12 13:43:47 +0100

    fix: change way to fetch user data

--- src/pages/UserRepos/UserInfo.jsx
+++ src/pages/UserRepos/UserInfo.jsx
@@ -3,27 +3,35 @@ import request from "../../helpers/request";
 import UserInfoCard from "../../components/UserInfoCard/UserInfoCard";
 import {Container} from "./UserInfo.css";
 
-//https://api.github.com/users/Adi52/repos
-
 const UserInfo = ({match}) => {
     const username = match.params.user;
 
     const [userInfo, setUserInfo] = useState({});
     const [reposList, setReposList] = useState({});
-
-
+    
+    
     useEffect(() => {
-        request.get(`/users/${username}/repos`)
+        request.get(`/users/${username}`)
             .then((response) => {
                 if (response && response.status === 404) {
                     console.clear();
                     setUserInfo('Invalid username!');
                 } else {
-                    setUserInfo(response.data[0].owner);
-                    setReposList(response.data);
+                    // When username is valid - fetch user repos
+                    setUserInfo(response.data);
+                    request.get(`/users/${username}/repos`)
+                        .then((response) => {
+                            if (response && response.status === 404) {
+                                console.clear();
+                            } else {
+                                setReposList(response.data);
+                            }
+                        })
+                        .catch()
                 }
             })
             .catch()
+
 
     }, [username])
 
