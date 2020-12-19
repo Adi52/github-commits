b049c3f369ed64790a393753750784c8478bc14d
Adi52 <adimr52@gmail.com>
2020-12-19 15:12:07 +0100

    fix: add smooth behavior to 'scrollToTop' component

--- src/components/ScrollToTop/ScrollToTop.jsx
+++ src/components/ScrollToTop/ScrollToTop.jsx
@@ -7,7 +7,11 @@ export default function ScrollToTop() {
     const { pathname } = useLocation();
 
     useEffect(() => {
-        window.scrollTo(0, 0);
+        window.scrollTo({
+            top: 0,
+            left: 0,
+            behavior: 'smooth',
+        });
     }, [pathname]);
 
     return null;
